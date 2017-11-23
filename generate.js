7a7c42b4bcaab71fc3d00a7f5dadab2a244c643f
Tomasz Dziuda <dziudek@gmail.com>
2017-11-23 11:21:42 +0100

    Small improvement in the code and example SVG file

--- generate.js
+++ generate.js
@@ -5,4 +5,4 @@ let fileContent = fs.readFileSync(__dirname + '/input/svg-map.svg');
 let svg2js = new SVG2JS(fileContent.toString());
 let result = svg2js.generateJson();
 
-fs.writeFileSync(__dirname + '/output/svg-map.js', 'window.publiiSvgFix = ' + JSON.stringify(result));
+fs.writeFileSync(__dirname + '/output/svg-map.js', 'window.publiiSvgFix = ' + JSON.stringify(result)) + ';';
